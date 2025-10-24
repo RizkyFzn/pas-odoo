5597426c7c0df791c0ade28010455dc9ed888107
Ahmad Rizky Fauzan <rfauzan211@gmail.com>
2025-10-24 17:23:45 +0700

    [ADD] - pas_material_request:add last purchase price and date

--- pas_material_request/models/purchase_request_line.py
+++ pas_material_request/models/purchase_request_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class PurchaseRequestLine(models.Model):
     _inherit = 'purchase.request.line'
@@ -10,3 +10,40 @@ class PurchaseRequestLine(models.Model):
         ondelete='cascade',
         help="Line dari Material Request yang memicu PR line ini"
     )
+
+    last_purchase_price = fields.Monetary(
+        string='Last Purchase Price',
+        compute='_compute_last_purchase_info',
+        store=True,
+        currency_field='currency_id',
+        help="Harga terakhir kali produk ini dibeli"
+    )
+
+    last_purchase_date = fields.Date(
+        string='Last Purchase Date',
+        compute='_compute_last_purchase_info',
+        store=True,
+        help="Tanggal terakhir kali produk ini dibeli"
+    )
+
+    @api.depends('product_id')
+    def _compute_last_purchase_info(self):
+        """Compute last purchase price and date from purchase order lines"""
+        for line in self:
+            if not line.product_id:
+                line.last_purchase_price = 0.0
+                line.last_purchase_date = False
+                continue
+
+            # Search for the last purchase order line for this product
+            last_po_line = self.env['purchase.order.line'].search([
+                ('product_id', '=', line.product_id.id),
+                ('order_id.state', 'in', ['purchase', 'done']),
+            ], order='date_order desc', limit=1)
+
+            if last_po_line:
+                line.last_purchase_price = last_po_line.price_unit
+                line.last_purchase_date = last_po_line.date_order.date()
+            else:
+                line.last_purchase_price = 0.0
+                line.last_purchase_date = False
